4b1532de2555b9370fddde0e39c4a20805c9a29d
EmanueleZii <241270998+emaZii@users.noreply.github.com>
2026-02-11 14:51:34 +0100

    prima della migrazione
    Non e tutto il modulo solo quello che serve per testare se funziona

---
+++ migrationtest/__manifest__.py
@@ -0,0 +1,58 @@
+
+{
+    'name': 'PIX - Solidali Base',
+    'version': '16.0.1.0.1',
+    'category': 'Base Solidali',
+    'description': """
+                Gestione Base per Solidali - Questo modulo fornisce un sistema integrato per la gestione di servizi di assistenza domiciliare, 
+                inclusi contratti, presenze, buste paga, contributi INPS e adempimenti fiscali
+                """,
+    'author': 'Pixora',
+    'website': 'http://www.pixora.it',
+    'license': 'Other proprietary',
+    'depends': [
+        'sale_subscription',
+        'hr_recruitment',
+        'hr_attendance',
+        'hr_payroll',
+        'web_refresher',
+        'report_py3o',
+        'hr_work_entry_contract'
+    ],
+    'data': [
+        'data/product.xml',
+        'data/ir_sequence_data.xml',
+        'security/solidali_security.xml',
+        'security/ir.model.access.csv',
+        'views/partner_view.xml',
+        'views/solidali_paghe_menuitems.xml',
+        'views/reports.xml',
+        'views/calendar_event_view.xml',
+        'views/solidali_paghe_mav_view.xml',
+        'views/solidali_paghe_ra_view.xml',
+        'views/solidali_paghe_view.xml',
+        'views/solidali_servizio_view.xml',
+        'views/solidali_titolo_studio_view.xml',
+        'views/hr_employee.xml',
+        'views/account_move_view.xml',
+        'views/subscription_id_view.xml'
+        'config/solidali_paghe_conf_fatture_view.xml',
+        'config/solidali_paghe_conf_indennita_view.xml',
+        'config/solidali_paghe_conf_ra_view.xml',
+        'config/solidali_paghe_conf_retribuzioni_view.xml',
+        'config/solidali_paghe_conf_tabella_inps_view.xml',
+        'config/solidali_paghe_conf_tessera_asnali_view.xml',
+        'config/solidali_paghe_indice_istat_view.xml',
+        'wizard/crea_mav.xml',
+        'wizard/create_bp.xml',
+        'wizard/filtra_abbonamento_wizard.xml',
+        'wizard/work_entry_duplicate.xml',
+        'views/sale_order_actions.xml'
+    ],
+    'demo': [
+        'data/demo_data.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ migrationtest/models/hr_contract.py
@@ -0,0 +1,19 @@
+from odoo import api, fields, models
+
+class Contract(models.Model):
+    _inherit = 'hr.contract'
+
+    subscription_id = fields.Many2one('sale.order', string="Abbonamento")
+
+    
+    @api.constrains('employee_id', 'state', 'kanban_state', 'date_start', 'date_end')
+    def _check_current_contract(self):
+        return True
+
+    def _generate_work_entries(self, date_start, date_stop, force=False):
+        for contract in self:
+            res = super(Contract, contract)._generate_work_entries(date_start, date_stop, force)
+            for work_entry in res:
+                work_entry.contract_id = contract.id
+                work_entry.subscription_id = contract.subscription_id.id    
+        return res
