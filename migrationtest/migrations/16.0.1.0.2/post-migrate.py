0e570f7d55d33b7f1f8feadfdc805dc524911030
EmanueleZii <241270998+emaZii@users.noreply.github.com>
2026-02-11 15:17:38 +0100

    Add files via upload

---
+++ migrationtest/migrations/16.0.1.0.2/post-migrate.py
@@ -0,0 +1,11 @@
+# migrations/16.0.1.1/post-migrate.py
+def migrate(cr, version):
+    # Aggiorniamo hr_version usando i dati salvati nella tabella temporanea
+    cr.execute("""
+        UPDATE hr_version v
+        SET subscription_id = t.subscription_id
+        FROM temp_subscription_move t
+        WHERE v.contract_id = t.old_contract_id
+    """)
+    # Pulizia: eliminiamo la tabella temporanea
+    cr.execute("DROP TABLE IF EXISTS temp_subscription_move")
