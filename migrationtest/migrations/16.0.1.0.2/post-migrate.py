995b2ab6d245b244cafa02d4777d49079f0a489c
EmanueleZii <241270998+emaZii@users.noreply.github.com>
2026-02-12 15:18:04 +0100

    Update post-migrate.py

--- migrationtest/migrations/16.0.1.0.2/post-migrate.py
+++ migrationtest/migrations/16.0.1.0.2/post-migrate.py
@@ -2,40 +2,31 @@ import logging
 _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
+    _logger.info("##################################################################################")    
     _logger.info("POST-MIGRATION: Generazione record in hr_version")
-
-    # 1. Verifichiamo se la tabella hr_version è stata creata dall'ORM
-    cr.execute("SELECT to_regclass('hr_version')")
-    if not cr.fetchone()[0]:
-        _logger.error("POST-MIGRATION: Tabella hr_version non trovata! Verifica i tuoi modelli Python.")
-        return
-
-    # 2. Inserimento massivo dei record
-    # Usiamo '1' per create_uid e write_uid (utente admin)
-    # NOW() per le date di creazione
-    cr.execute("""
-        INSERT INTO hr_version (
-            contract_id, 
-            subscription_id, 
-            create_uid, 
-            write_uid, 
-            create_date, 
-            write_date
-        )
-        SELECT 
-            contract_id, 
-            subscription_id, 
-            1, 
-            1, 
-            NOW(), 
-            NOW()
-        FROM temp_hr_version_data
-        WHERE contract_id NOT IN (SELECT contract_id FROM hr_version)
-    """)
+    _logger.info("##################################################################################")
     
-    count = cr.rowcount
-    _logger.info(f"POST-MIGRATION: Creati {count} nuovi record in hr_version")
-
+    # 1. Verifichiamo se la tabella hr_version è stata creata dall'ORM
+    #cr.execute("SELECT to_regclass('hr_version')")
+    #if not cr.fetchone()[0]:
+    #    _logger.error("POST-MIGRATION: Tabella hr_version non trovata! Verifica i tuoi modelli Python.")
+    #    return
+    
+    cr.execute("SELECT 1 FROM information_schema.tables WHERE table_name='temp_hr_version_data'")
+    # 2. Inserimento massivo dei record
+    if cr.fetchone():
+        cr.execute("""
+            INSERT INTO hr_version (contract_id, subscription_id, create_date, write_date)
+            SELECT contract_id, subscription_id, NOW(), NOW()
+            FROM temp_hr_version_data
+            WHERE contract_id NOT IN (SELECT contract_id FROM hr_version)
+        """)
+        
+    
     # 3. Pulizia
+    _logger.info("##################################################################################")
+    
     cr.execute("DROP TABLE IF EXISTS temp_hr_version_data")
     _logger.info("POST-MIGRATION: Pulizia tabella temporanea completata.")
+    _logger.info("##################################################################################")
+    
