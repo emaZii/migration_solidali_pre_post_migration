db4dd7d8bdbbb8030e1b224b6ec2a3b9f4db99d7
EmanueleZii <241270998+emaZii@users.noreply.github.com>
2026-02-12 15:18:24 +0100

    Update pre-migrate.py

--- migrationtest/migrations/16.0.1.0.2/pre-migrate.py
+++ migrationtest/migrations/16.0.1.0.2/pre-migrate.py
@@ -2,15 +2,18 @@ import logging
 _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
+    _logger.info("##################################################################################")
     _logger.info("PRE-MIGRATION: Salvataggio dati per creazione hr.version")
+    _logger.info("##################################################################################")
     
     # Creiamo la tabella temporanea per trasportare i dati tra le fasi
     cr.execute("""
         CREATE TABLE IF NOT EXISTS temp_hr_version_data AS
-        SELECT 
-            id as contract_id, 
-            subscription_id
+        SELECT id AS contract_id, subscription_id
         FROM hr_contract
         WHERE subscription_id IS NOT NULL
     """)
+    _logger.info("##################################################################################")
     _logger.info("PRE-MIGRATION: Dati pronti in temp_hr_version_data")
+    _logger.info("##################################################################################")
+    
